85d46a3803b8ab8a3ab75e71e31c24722f98eb42
Riyadhusshadiqin <Riyadhus25@gmail.com>
2022-11-09 13:13:41 +0700

    Memasukkan kelas Teh dan Kopi

---
+++ Teh.java
@@ -0,0 +1,55 @@
+/**
+ * @author Riyadhusshadiqin
+ * @version 1.0
+ */
+
+ /**
+  * Mengimport scanner untuk mengambil inputan dari user
+  */
+  import java.util.Scanner;
+
+  public class Teh extends Minuman
+  {   
+      public static double runningTotal = 0;
+      static Scanner input = new Scanner(System.in);
+      
+      /**
+       * @method contructor untuk Teh
+       */
+      public Teh(){
+          super(5000);
+      }
+  
+      /**
+       * @method subtotal untuk menghitung subtotal dari minuman
+       * @param quantity adalah jumlah dari minuman
+       * @param itemPrice adalah harga dari minuman
+       * @return subTotal mengembalikan subTotal yang telah dihitung
+       */
+      public double subTotal(double quantity, double itemPrice){
+      double subTotal = super.getquantity()*super.getitemprice();
+      System.out.println("Subtotal: "+ subTotal);
+      runningTotal += subTotal;
+      return subTotal;
+      }
+      
+      /**
+       * @method quantity untuk meminta jumlah minuman dari user
+       * @return quantity mengembalikan jumlah minuman yang telah diinput user
+       */
+      public double quantity() {
+      System.out.print("Masukkan jumlah: ");       
+      double quantity = input.nextDouble();
+      super.setquantity(quantity);
+      subTotal(super.getquantity(), super.getitemprice());
+      return quantity;
+      }
+      
+      /**
+       * @method getrunningtotal untuk menyimpan nilai dari subtotal
+       * @return runningTotal mengembalikan total dari minuman
+       */
+      public double getrunningtotal(){
+          return runningTotal;
+      }
+  }
